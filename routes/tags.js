1673ebd2b6ba867e8d9de0310d56749724e7f7a3
Valerie TV <vtylskivincent@gmail.com>
2021-01-11 19:19:11 +0100

    add a road to get all posts related to a special tag

--- routes/tags.js
+++ routes/tags.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const postsTags = require('../data/tags');
+const allTags = require('../data/tags');
+const allPosts = require('../data/posts');
 
 const router = express.Router();
 
 // Get a list of tags
 router.get('/', (req, res) => {
-  res.json(postsTags);
+  res.json(allTags);
+});
+
+// Bonus: get a list of posts related to a special tag id
+router.get('/:tagId/posts', (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const foundTag = allPosts.filter((post) => post.tags_id.includes(tagId));
+  if (!foundTag) {
+    return res.status(404).json({
+      error: 'Post not found',
+    });
+  }
+  return res.json(foundTag);
 });
 
 module.exports = router;
